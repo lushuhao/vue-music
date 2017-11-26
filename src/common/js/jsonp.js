f8718f30597b9afd1f793f42658c9c92496f3183
lushuhao <shahramlu@163.com>
2017-11-26 16:31:53 +0800

    封装jsonp

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,26 @@
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') === -1 ? '?' : '&') + param(data); // 判断是否有?号
+
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+function param(data) {
+  let url = '';
+  for (let i in data) {
+    if (data.hasOwnProperty(i)){
+      let value = data[i] !== undefined ? data[i] : '';
+      url += `&${i}=${encodeURIComponent(value)}`
+    }
+  }
+  return url ? url.substring(1) : ''  // 删除第一个&，不确定是否有？
+}
