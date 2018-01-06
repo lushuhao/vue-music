db1228bfe4024c2a7a1c675b83503d4cd0b8464e
lushuhao <shahramlu@163.com>
2018-01-06 15:22:14 +0800

    播放器所需要的state数据

---
+++ src/common/js/config.js
@@ -0,0 +1,5 @@
+export const playMode = { // 播放模式，顺序、单曲、随机
+  sequence: 0,
+  loop: 1,
+  random: 2
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import {playMode} from 'common/js/config';
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false, // 播放状态
+  fullScreen: false, // 全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 顺序播放列表
+  playMode: playMode.sequence, // 播放模式
+  currentIndex: -1, // 当前播放索引
 }
 
 export default state
