0d9d34b87e2815defc5b579d3a0afcc3f7864295
lushuhao <shahramlu@163.com>
2018-01-14 19:31:24 +0800

    播放器最大最小化，样式调整

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -2,20 +2,20 @@
   <div class="player" v-show="playList.length">
     <div class="normal-player" v-show="fullScreen">
       <div class="background">
-        <img src="" alt="" width="100%" height="100%"/>
+        <img :src="currentSong.image" width="100%" height="100%"/>
       </div>
       <div class="top">
-        <div class="back">
+        <div class="back" @click="back">
           <i class="icon-back"></i>
         </div>
-        <h1 class="title"></h1>
-        <h2 class="subTitle"></h2>
+        <h1 class="title" v-html="currentSong.name"></h1>
+        <h2 class="subTitle" v-html="currentSong.singer"></h2>
       </div>
       <div class="middle">
         <div class="middle-l">
           <div class="cd-wrapper">
             <div class="cd">
-              <img class="image" src="" alt=""/>
+              <img class="image" :src="currentSong.image"/>
             </div>
           </div>
         </div>
@@ -30,13 +30,13 @@
         </div>
       </div>
     </div>
-    <div class="mini-player" v-show="!fullScreen">
+    <div class="mini-player" v-show="!fullScreen" @click="open">
       <div class="icon">
-        <img width="40" height="40" src="" alt="">
+        <img width="40" height="40" :src="currentSong.image"/>
       </div>
       <div class="text">
-        <h2 class="name"></h2>
-        <p class="desc"></p>
+        <h2 class="name" v-html="currentSong.name"></h2>
+        <p class="desc" v-html="currentSong.singer"></p>
       </div>
       <div class="control"></div>
       <div class="control">
@@ -47,14 +47,27 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapState} from 'vuex'
+  import {mapState, mapGetters, mapMutations} from 'vuex'
+  import * as types from '../../store/mutation-types.js'
 
   export default {
     computed: {
       ...mapState([
         'fullScreen',
         'playList'
-      ])
+      ]),
+      ...mapGetters(['currentSong'])
+    },
+    methods: {
+      ...mapMutations({
+        setFullScreen: types.SET_FULL_SCREEN
+      }),
+      back() {
+        this.setFullScreen(false)
+      },
+      open() {
+        this.setFullScreen(true)
+      }
     }
   }
 </script>
@@ -101,6 +114,22 @@
             transform: rotate(-90deg)
           }
         }
+
+        .title {
+          width: 70%
+          margin: 0 auto
+          line-height: 40px
+          text-align: center
+          no-wrap()
+          font-size: $font-size-large
+          color: $color-text
+        }
+        .subTitle {
+          line-height: 20px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-text
+        }
       }
 
       .middle {
@@ -121,21 +150,21 @@
             top: 0
             width: 80%
             height: 100%
+            box-sizing: border-box
 
             .cd {
               width: 100%
               height: 100%
-              box-sizing: border-box
-              border: 10px solid rgba(255, 255, 255, .1)
               border-radius: 50%
 
-              .image{
+              .image {
                 position: absolute
                 top: 0
                 left: 0
                 width: 100%
                 height: 100%
                 border-radius: 50%
+                border: 10px solid rgba(255, 255, 255, .1)
               }
             }
           }
@@ -167,6 +196,67 @@
         }
       }
 
+    }
+
+    .mini-player {
+      display: flex
+      align-items: center
+      position: fixed
+      left: 0
+      bottom: 0
+      z-index: 180
+      width: 100%
+      height: 60px
+      background: $color-highlight-background
+
+      .icon {
+        flex: 0 0 40px
+        width: 40px
+        padding: 0 10px 0 20px
+
+        img {
+          border-radius: 50%
+        }
+      }
+
+      .text {
+        display: flex
+        flex-direction: column
+        justify-content: center
+        flex: 1
+        line-height: 20px
+        overflow: hidden
+
+        .name {
+          margin-bottom: 2px
+          no-wrap()
+          font-size: $font-size-medium
+          color: $color-text
+        }
+
+        .desc {
+          no-wrap()
+          font-size: $font-size-small
+          color: $color-text-d
+        }
+      }
+
+      .control {
+        flex: 0 0 30px
+        width: 30px
+        padding: 0 10px
+
+        .icon-play-mini, .icon-pause-mini, .icon-playlist {
+          font-size: 30px
+          color: $color-theme-d
+        }
+        .icon-mini{
+          font-size: 32px
+          position: absolute
+          left: 0
+          top: 0
+        }
+      }
     }
   }
 </style>
