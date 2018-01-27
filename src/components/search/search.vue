16be719e47b639d99647c403f94f4258cfc482be
lushuhao <shahramlu@163.com>
2018-01-27 20:04:18 +0800

    父子组件交互，子组件通过emit，父组件通过refs调用子组件的事件

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="search">
     <div class="search-box-wrapper">
-      <search-box></search-box>
+      <search-box ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li @click="addQuery(item.k)" class="item" v-for="item in hotKey">{{item.k}}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,13 +21,21 @@
   import {getHotKey} from 'api/search'
 
   export default {
+    data() {
+      return {
+        hotKey: []
+      }
+    },
     created() {
       this._getHotKey()
     },
     methods: {
+      addQuery(query) {
+        this.$refs.searchBox.setQuery(query)
+      },
       _getHotKey() {
         getHotKey().then(res => {
-          console.log(res)
+          this.hotKey = res.hotkey.slice(0, 10)
         })
       }
     },
@@ -34,6 +52,38 @@
 
     .search-box-wrapper {
       margin: 20px
+    }
+
+    .shortcut-wrapper{
+      position: fixed
+      top: 178px
+      bottom: 0
+      width: 100%
+
+      .shortcut{
+        height: 100%
+        overflow: hidden
+
+        .hot-key{
+          margin: 0 20px 20px 20px
+
+          .title{
+            margin-bottom: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+          }
+
+          .item{
+            display: inline-block
+            padding: 5px 10px
+            margin: 0 20px 10px 0
+            border-radius: 6px
+            background: $color-highlight-background
+            font-size: $font-size-medium
+            color: $color-text-d
+          }
+        }
+      }
     }
 
   }
