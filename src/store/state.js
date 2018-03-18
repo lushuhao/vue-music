49418b11f77b2af2a8daa2b3f8d39dc6d2df49a8
lushuhao <shahramlu@163.com>
2018-03-18 21:37:48 +0800

    header添加播放入口

--- src/store/state.js
+++ src/store/state.js
@@ -1,15 +1,25 @@
 import {playMode} from 'common/js/config'
 import {getInitSearch, loadPlay} from 'common/js/cache'
 
+const currentSong = loadPlay().length && loadPlay()[0]
+
+const currentIndex = loadPlay().findIndex(item => {
+  if (item && currentSong) {
+    return item.id === currentSong.id
+  } else {
+    return -1
+  }
+})
+
 const state = {
   singer: {}, // 歌手详情页
   playing: false, // 播放状态
   fullScreen: false, // 全屏
-  playList: [], // 播放列表
-  sequenceList: [], // 顺序播放列表
+  playList: loadPlay(), // 播放列表
+  sequenceList: loadPlay(), // 顺序播放列表
   playMode: playMode.sequence, // 播放模式
-  currentIndex: -1, // 当前播放索引
-  currentSong: '', // 当前播放歌曲
+  currentIndex, // 当前播放索引
+  currentSong, // 当前播放歌曲
   disc: {}, // 歌单详情页
   topList: {}, // 排行榜详情页
   searchHistory: getInitSearch(), // 搜索历史
